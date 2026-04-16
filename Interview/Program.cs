63e2aef0a80d23974515d9bda18ca54c2220cc3b
yepolonais <yepo@hotmail.fr>
2026-04-16 17:51:03 +0200

    dotnet 8

--- Interview/Program.cs
+++ Interview/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 // Application DI
@@ -19,7 +18,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
